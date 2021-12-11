c50d339a5d9851770ab5f2447f1e2bfd07951dad
ShrutiJ-01 <shruti.jawale09@gmail.com>
2021-12-11 07:53:15 +0530

    Changed function get MilestonesforProject() in accordance with new schema

--- src/main/java/com/example/productivize/commandLineInterface.java
+++ src/main/java/com/example/productivize/commandLineInterface.java
@@ -41,11 +41,11 @@ public class commandLineInterface {
         Employee emp2=authenticate.loginEmployee(emp.id, "new");
         log.info("employee logged in is : "+emp2.first_name+" "+emp2.last_name);
 
-        // Manager manager1=authenticate.registerManager("singleton", "pattern", "new");
-        // log.info("Id of the registration " + manager1.id);
-        // log.info("LOGIN FOR MANAGER");
-        // Manager manager2=authenticate.loginManager(manager1.id, "new");
-        // log.info("manager logged in is : "+manager2.first_name+" "+manager2.last_name);
+        Manager manager1=authenticate.registerManager("singleton", "pattern", "new");
+        log.info("Id of the registration " + manager1.id);
+        log.info("LOGIN FOR MANAGER");
+        Manager manager2=authenticate.loginManager(manager1.id, "new");
+        log.info("manager logged in is : "+manager2.first_name+" "+manager2.last_name);
 
 
         log.info("---------------------------------------------------------");
@@ -57,19 +57,19 @@ public class commandLineInterface {
         boolean res=employeeDao.delete(employee.id);
         log.info("Delete succesful ? : "+res);
 
-        // log.info("---------------------------------------------------------");
-        // log.info("PROJECTSDAO.JAVA ");   
-        // Project project=new Project("kijugo", "05/11/21","08/11/21", manager1.id, 1);
-        // ProjectDao projectDao=new ProjectDao();
-        // result= projectDao.insert(project);
-        // log.info("Insert succesful ? : "+result);
-        // result=projectDao.update(project);
-        // log.info("Update succesful ? : "+result); 
-        // result=projectDao.delete(project.id);
-        // log.info("Delete succesful ? : "+result);
-        // log.info("Querying ongoing projects for manager_id 12 | Entries expected : 1 ");
-        // queryResultSet=projectDao.getOngoingProjectsForManager(207);
-        // log.info("Entries found :  "+queryResultSet.getRow());
+        log.info("---------------------------------------------------------");
+        log.info("PROJECTSDAO.JAVA ");   
+        Project project=new Project("kijugo", "05/11/21","08/11/21", manager1.id, 1);
+        ProjectDao projectDao=new ProjectDao();
+        result= projectDao.insert(project);
+        log.info("Insert succesful ? : "+result);
+        result=projectDao.update(project);
+        log.info("Update succesful ? : "+result); 
+        result=projectDao.delete(project.id);
+        log.info("Delete succesful ? : "+result);
+        log.info("Querying ongoing projects for manager_id 997 | Entries expected : 1 ");
+        queryResultSet=projectDao.getOngoingProjectsForManager(997);
+        log.info("Entries found :  "+queryResultSet.getRow());
         // log.info("Querying blocked projects for manager_id 12 | Entries expected : 1 ");
         // queryResultSet=projectDao.getBlockedProjectsForManager(207);
         // log.info("Entries found :  "+queryResultSet.getRow());
@@ -77,33 +77,33 @@ public class commandLineInterface {
         // queryResultSet=projectDao.getCompletedProjectsForManager(207);
         // log.info("Entries found :  "+queryResultSet.getRow());
 
-        // log.info("---------------------------------------------------------");
-        // log.info("MILESTONESDAO.JAVA ");   
-        // Milestone milestone=new Milestone("Release Build", "09/09/21", "create release build", 11, 2);
-        // MilestonesDao milestoneDao=new MilestonesDao();
-        // result= milestoneDao.insert(milestone);
-        // log.info("Insert succesful ? : "+result);
-        // milestone.name="updated name of milestone";
-        // result=milestoneDao.update(milestone);
-        // log.info("Update succesful ? : "+result); 
-        // result=milestoneDao.delete(milestone.id);
-        // log.info("Delete succesful ? : "+result);
-        // log.info("Querying Milestones for project_id 12 | Entries expected : 2 ");
-        // queryResultSet= milestoneDao.getMilestonesForProject(12);
-        // log.info("Entries found :  "+queryResultSet.getRow());
+        log.info("---------------------------------------------------------");
+        log.info("MILESTONESDAO.JAVA ");   
+        Milestone milestone=new Milestone("Release Build", "09/09/21", "create release build", 500, 2);
+        MilestonesDao milestoneDao=new MilestonesDao();
+        result= milestoneDao.insert(milestone);
+        log.info("Insert succesful ? : "+result);
+        milestone.name="updated name of milestone";
+        result=milestoneDao.update(milestone);
+        log.info("Update succesful ? : "+result); 
+        result=milestoneDao.delete(milestone.id);
+        log.info("Delete succesful ? : "+result);
+        log.info("Querying Milestones for project_id 12 | Entries expected : 2 ");
+        queryResultSet= milestoneDao.getMilestonesForProject(500);
+        log.info("Entries found :  "+queryResultSet.getRow());
 
 
-        // log.info("---------------------------------------------------------");
-        // log.info("TASKSDAO.JAVA ");   
-        // Task task=new Task("check employee dao", 180, 2, 301);
-        // TaskDao taskDao=new TaskDao();
-        // result=taskDao.insert(task);
-        // log.info("Insert succesful ? : "+result); 
-        // task.name="updated task name";
-        // result=taskDao.update(task);  
-        // log.info("Update succesful ? : "+result); 
-        // result=taskDao.delete(task.id);
-        // log.info("Delete succesful ? : "+result);
+        log.info("---------------------------------------------------------");
+        log.info("TASKSDAO.JAVA ");   
+        Task task=new Task("check employee dao", 180, 2, 301);
+        TaskDao taskDao=new TaskDao();
+        result=taskDao.insert(task);
+        log.info("Insert succesful ? : "+result); 
+        task.name="updated task name";
+        result=taskDao.update(task);  
+        log.info("Update succesful ? : "+result); 
+        result=taskDao.delete(task.id);
+        log.info("Delete succesful ? : "+result);
 
         log.info("---------------------------------------------------------");
         log.info("WORKLOGDAO.JAVA ");   
